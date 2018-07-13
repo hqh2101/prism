a4492c330cf75745767d727cb61b7698ffb91532
Huy Ha <haquochuy@gmail.com>
2018-07-13 00:02:13 -0400

    turned it into drawing program. Change brush size feature, export feature, responsive canvas (but not responsive drawing

--- js/prism.js
+++ js/prism.js
@@ -1,27 +1,35 @@
-var width,height;
+//TODO:
+//Responsive
+//ControlZ + array implementaton + redraw everytime
+//mouse circle
+//
+var brushSize;
+var strokes = [];
 
 function setup(){
 	createCanvas(windowWidth, windowHeight);
 	background('black');
-	console.log("created canvas");
 	noLoop();
-	width = windowWidth;
-	height = windowHeight;
+  brushSize = 30;
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  background('black');
 }
 
 function drawTri(){
-	let x1 = Math.floor(Math.random()*width);
-	let x2 = Math.floor(Math.random()*width);
-	let x3 = Math.floor(Math.random()*width);
-	let y1 = Math.floor(Math.random()*height);
-	let y2 = Math.floor(Math.random()*height);
-	let y3 = Math.floor(Math.random()*height);
+  //random triangle vertices
+	let x1 = mouseX + Math.floor((0.5-Math.random())*brushSize);
+	let x2 = mouseX + Math.floor((0.5-Math.random())*brushSize);
+	let x3 = mouseX + Math.floor((0.5-Math.random())*brushSize);
+	let y1 = mouseY + Math.floor((0.5-Math.random())*brushSize);
+	let y2 = mouseY + Math.floor((0.5-Math.random())*brushSize);
+	let y3 = mouseY + Math.floor((0.5-Math.random())*brushSize);
 	noStroke();
+
 	fill('rgba('+rndColor()+','+rndColor()+','+rndColor()+',0.1)');
+  //draw triangle
 	triangle(x1, y1, x2, y2, x3, y3);
 }
 
@@ -34,11 +42,33 @@ function rndColor(){
 	return Math.floor(Math.random()*255);
 }
 
-function keyTyped(){
-	if(key === ' ')
-		draw();
+function draw(){
+  //TODO allow user to change this number somehow
+	drawMultipleTriangles(5);
 }
 
-function draw(){
-	drawMultipleTriangles(100);
+function mousePressed(){
+  draw();
 }
+
+function mouseDragged(){
+  draw();
+}
+
+
+//change brush weight
+function mouseWheel(){
+  if(event.delta<0)
+    brushSize += 10;
+  else{
+    if(brushSize>10)
+      brushSize -=10;
+  }
+}
+
+//save image
+var saveButton = document.getElementById("save-button");
+
+saveButton.onclick = function(){
+  saveCanvas('myPrism', 'jpg');
+};
