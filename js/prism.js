c8224fb453949cce6dde8247c54ebd6da94c2d42
Huy Ha <haquochuy@gmail.com>
2018-07-13 19:47:40 -0400

    Successfully implemented array to store strokes, but error on drawing strokes, and very low performance

--- js/prism.js
+++ js/prism.js
@@ -2,13 +2,19 @@
 //Responsive
 //ControlZ + array implementaton + redraw everytime
 //mouse circle
-var brushSize;
+//change color
+//resize but canvas is still ok
+//Hide mouse while exporting
+//Performance: create a jpeg every few strokes to limt ctrl z and limit speed, then replace background image
+var brushSize,currentStroke;
 var strokes = [];
+
 
 function setup(){
 	createCanvas(windowWidth, windowHeight);
 	background('black');
   brushSize = 30;
+  currentStroke = 0;
 }
 
 function windowResized() {
@@ -16,7 +22,7 @@ function windowResized() {
   background('black');
 }
 
-function drawTri(){
+function tri(){
   //random triangle vertices
 	let x1 = mouseX + Math.floor((0.5-Math.random())*brushSize);
 	let x2 = mouseX + Math.floor((0.5-Math.random())*brushSize);
@@ -37,19 +43,43 @@ function rndColor(){
 
 function draw(){
   background(0);
+  drawStrokes();
   //draw background
   //draw all triangles in array
   //draw circle around mouse
   mouseCircle();
 }
 
-// function mousePressed(){
-//   draw();
-// }
+//_______________________________________________draw code
 
-// function mouseDragged(){
-//   draw();
-// }
+function drawStrokes(){
+  for(let i = 0; i < strokes.length;i++){
+    for(let j = 0; j < strokes[i].length;j++){
+      fill(strokes[i][j][6]);
+      triangle(strokes[i][j][0],
+        strokes[i][j][1],
+        strokes[i][j][2],
+        strokes[i][j][3],
+        strokes[i][j][4],
+        strokes[i][j][5],);
+    }
+  }
+}
+
+function mousePressed(){
+  var newStroke = [];
+  strokes.push(newStroke);
+  if(strokes.length !== currentStroke)
+    console.log("Stroke error");
+}
+
+function mouseDragged(){
+  strokes[currentStroke].push(tri());
+}
+
+function mouseReleased(){
+  currentStroke++;
+}
 
 function handleStroke(){
   var stroke = [];
@@ -58,6 +88,8 @@ function handleStroke(){
   }
   strokes.push(stroke);
 }
+
+//_____________________________________________end of draw code
 
 //change brush weight
 function mouseWheel(){
@@ -84,5 +116,4 @@ function mouseCircle(){
     noFill();
     ellipse(mouseX,mouseY,brushSize,brushSize);
   pop();
-  console.log("drawing mouse circle");
 }
